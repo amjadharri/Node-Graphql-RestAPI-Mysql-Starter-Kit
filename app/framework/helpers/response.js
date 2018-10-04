38f7a5e0e80682eb26180d59cea13d834b7aa273
Ilyas Karim <ilyas.datoo@gmail.com>
2018-10-04 23:49:54 +0500

    Improved response in rest api

--- app/framework/helpers/response.js
+++ app/framework/helpers/response.js
@@ -1,5 +1,10 @@
 import statusCodes from 'http-status-codes';
 export default function (obj) {
   let { data, req, res, body } = obj;
+  if (data.hasOwnProperty('statusCode')) {
+    res.json(data);
+    return;
+  }
   res.status(statusCodes[data.statusCode]).json(data);
+  return;
 }
